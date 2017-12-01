bcbef5230ee24b395f6482d3a3aac3d4bfc4dd9e
Sebastian Werner <s.werner@sebastian-software.de>
2017-12-01 23:55:01 +0100

    Worked on prettier jest integration

---
+++ jest.config.prettier.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/unambiguous, import/no-commonjs */
+const readable = require(".")
+
+module.exports = {
+  runner: "jest-runner-prettier",
+  testRegex: ".(mjs|js|jsx|json|css|less|scss|pcss|md|markdown)$",
+  testPathIgnorePatterns: [ "/node_modules/", "package-lock.json" ].concat(readable.gitIgnores)
+}
