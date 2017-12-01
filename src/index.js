bea97dffb0f8f0469361ef480663c7ae18f39adf
Sebastian Werner <s.werner@sebastian-software.de>
2017-12-01 21:47:05 +0100

    Exporting methods + new helpers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function clean() {
+import spawn from "cross-spawn"
+
+export const execSync = spawn.sync
+
+export function getGitFiles(regexp) {
+  var gitFiles = execSync("git", [ "ls-files" ], { stdio: "pipe" })
+    .stdout.toString()
+    .trim()
+    .split("\n")
+
+  return gitFiles.filter((fileName) => {
+    return regexp.exec(fileName)
+  })
+}
+
+export function clean() {
   console.log(
-    execSync("git", ["clean", "--force"], { stdio: "pipe" }).stdout.toString()
+    execSync("git", [ "clean", "--force" ], { stdio: "pipe" }).stdout.toString()
   )
 }
 
-function cleanFull() {
+export function cleanFull() {
   console.log(
-    execSync("git", ["clean", "--force", "-x"], {
+    execSync("git", [ "clean", "--force", "-x" ], {
       stdio: "pipe"
     }).stdout.toString()
   )
