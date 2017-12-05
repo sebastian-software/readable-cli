cd816a3531f29e6e8ad705d4739ec03a90ab3528
Sebastian Werner <s.werner@sebastian-software.de>
2017-12-05 15:05:57 +0100

    Cleanup

--- jest.config.eslint.js
+++ jest.config.eslint.js
@@ -5,10 +5,5 @@ module.exports = {
   runner: "jest-runner-eslint",
   displayName: "lint:eslint",
   testRegex: ".(mjs|js|jsx)$",
-  testPathIgnorePatterns: ["/node_modules/"].concat(readable.gitIgnores),
-
-  // https://github.com/jest-community/jest-runner-eslint#clioptions
-  cliOptions: {
-    fix: true
-  }
+  testPathIgnorePatterns: [ "/node_modules/" ].concat(readable.gitIgnores)
 }
