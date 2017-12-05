0371a11c651cc30225700030ca93900c37a87d0b
Sebastian Werner <s.werner@sebastian-software.de>
2017-12-05 12:26:35 +0100

    Removed prettier jest runner... which is not doing what we like. + Added 'fix' flag for eslint.

--- jest.config.eslint.js
+++ jest.config.eslint.js
@@ -5,5 +5,10 @@ module.exports = {
   runner: "jest-runner-eslint",
   displayName: "lint:eslint",
   testRegex: ".(mjs|js|jsx)$",
-  testPathIgnorePatterns: [ "/node_modules/" ].concat(readable.gitIgnores)
+  testPathIgnorePatterns: ["/node_modules/"].concat(readable.gitIgnores),
+
+  // https://github.com/jest-community/jest-runner-eslint#clioptions
+  cliOptions: {
+    fix: true
+  }
 }
