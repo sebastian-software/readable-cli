779d1bdb25a930804db0d042ef3746a876012184
Sebastian Werner <s.werner@sebastian-software.de>
2017-12-08 18:11:33 +0100

    Added manual app-root-dir based rootDir for jest... to fix lookup when tool is installed.

--- jest.config.eslint.js
+++ jest.config.eslint.js
@@ -1,9 +1,11 @@
 /* eslint-disable import/no-commonjs */
 const { getScriptFiles, mapToJestRoot } = require(".")
+const root = require("app-root-dir").get()
 const files = getScriptFiles()
 
 module.exports = {
   runner: "jest-runner-eslint",
+  rootDir: root,
   testMatch: files ? files.map(mapToJestRoot) : [ "<rootDir>/-no-matching-git-files-found-" ],
   moduleFileExtensions: [
     "js",
--- jest.config.stylelint.js
+++ jest.config.stylelint.js
@@ -1,9 +1,11 @@
 /* eslint-disable import/no-commonjs */
 const { getStyleFiles, mapToJestRoot } = require(".")
+const root = require("app-root-dir").get()
 const files = getStyleFiles()
 
 module.exports = {
   runner: "jest-runner-stylelint",
+  rootDir: root,
   testMatch: files ? files.map(mapToJestRoot) : [ "<rootDir>/-no-matching-git-files-found-" ],
   moduleFileExtensions: [
     "css",
